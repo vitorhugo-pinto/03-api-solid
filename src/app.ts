54916254b3aad4e6e50a5c1a57985759bf9e7986
Vitor Hugo Pinto <vhpinto1@gmail.com>
2024-06-01 08:13:35 -0300

    feat: error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
 import fastify from 'fastify'
 import { routes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(routes)
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      error: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'prod') {
+    console.error(error)
+  } else {
+    // TODO: log error to a external logger system
+  }
+
+  return reply.status(500).send()
+})
