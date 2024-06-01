498bc663d30bd91db7669437b63d62993b951589
Vitor Hugo Pinto <vhpinto1@gmail.com>
2024-06-01 07:37:24 -0300

    feat: controller with repo and error

--- src/http/controllers/users-controller.ts
+++ src/http/controllers/users-controller.ts
@@ -1,6 +1,8 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
-import { createUserUseCase } from '@/use-cases/create-user'
+import { CreateUserUseCase } from '@/use-cases/create-user'
+import { PrismaUsersRepository } from '@/repositories/prisma-repositories/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function createUser(request: FastifyRequest, reply: FastifyReply) {
   const createUserBodySchema = z.object({
@@ -12,10 +14,18 @@ export async function createUser(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = createUserBodySchema.parse(request.body)
 
   try {
-    await createUserUseCase({ name, email, password })
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const createUserUseCase = new CreateUserUseCase(prismaUsersRepository)
+
+    await createUserUseCase.execute({ name, email, password })
   } catch (error) {
     console.error(error)
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({
+        message: error.message,
+      })
+    }
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
