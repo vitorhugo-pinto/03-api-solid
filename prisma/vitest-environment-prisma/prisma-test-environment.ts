d8dbcccec7c232d797118c989bc41e041a896fd1
Vitor Hugo Pinto <vhpinto1@gmail.com>
2024-08-04 21:19:25 -0300

    chore: setup db for e2e tests

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,42 @@
+import 'dotenv/config'
+
 import { Environment } from 'vitest'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDBUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('No database url was provided.')
+  }
+
+  const dbUrl = new URL(process.env.DATABASE_URL)
+
+  dbUrl.searchParams.set('schema', schema)
+
+  return dbUrl.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('Setup')
+    const randomSchema = randomUUID()
+    const databaseURL = generateDBUrl(randomSchema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
-        console.log('Teardown')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${randomSchema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
       },
     }
   },
