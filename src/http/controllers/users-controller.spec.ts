3e19848cce11da46333ecd2c06241309de15a1b2
Vitor Hugo Pinto <vhpinto1@gmail.com>
2024-08-06 22:24:44 -0300

    chore: supertest; feat: user controller test

--- src/http/controllers/users-controller.spec.ts
+++ src/http/controllers/users-controller.spec.ts
@@ -1,3 +1,22 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
 
-test('ok', () => {})
+describe('User controller', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should successfully create a user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Test name',
+      email: 'test@name.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
