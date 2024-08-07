bbac4d3fe48e541937f32096138aabb17a350c81
Vitor Hugo Pinto <vhpinto1@gmail.com>
2024-08-07 05:59:27 -0300

    feat: test authenticate user

--- src/http/controllers/authenticate-controller.spec.ts
+++ src/http/controllers/authenticate-controller.spec.ts
@@ -1,3 +1,30 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
 
-test('ok', () => {})
+describe('Authenticate controller', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should successfully authenticate a user', async () => {
+    await request(app.server).post('/users').send({
+      name: 'Test name',
+      email: 'test@name.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/authenticate').send({
+      email: 'test@name.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
